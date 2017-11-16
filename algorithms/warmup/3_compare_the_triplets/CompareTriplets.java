0f6a785ea36f2ba3752bb5bd18d4b64af8f84f01
Guimarães Neto <guimaraesneto@gmail.com>
2017-11-16 08:19:17 -0200

    Resolution of the third exercise of the algorithm. Language used java.

---
+++ algorithms/warmup/3_compare_the_triplets/CompareTriplets.java
@@ -0,0 +1,42 @@
+package br.com.warmuo;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2){
+        // Complete this function
+    		int[] result = {0,0};
+    		int[] a = {a0, a1, a2};
+    		int[] b = {b0, b1, b2};
+    		for (int i = 0; i < b.length; i++) {
+    			if (a[i] > b[i]) {
+    				result[0] += 1;
+			}else if (a[i] < b[i]) {
+				result[1] += 1;
+			}
+		}
+    		return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a0 = in.nextInt();
+        int a1 = in.nextInt();
+        int a2 = in.nextInt();
+        int b0 = in.nextInt();
+        int b1 = in.nextInt();
+        int b2 = in.nextInt();
+        int[] result = solve(a0, a1, a2, b0, b1, b2);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+        }
+        System.out.println("");
+        
+
+    }
+}
